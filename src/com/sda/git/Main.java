9cc5b90be5197c9446612ab1dd5d8fd5b4ce014c
Wik81 <wik99@wp.pl>
2018-10-21 12:51:56 +0200

    zmiana w main i w CustomList

--- src/com/sda/git/Main.java
+++ src/com/sda/git/Main.java
@@ -4,6 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
 
+        CustomList<String> lista = new CustomList<>();
+
+        lista.add("Jan");
+        lista.add("Maciej");
+        lista.add("Jacek");
+        lista.add("Anka");
+        lista.add("Hanka");
+        lista.add("Jozek");
+        lista.add("Bartek");
+        lista.add("Piotr");
+        lista.add("Janusz");
+        lista.add("Andrzej");
+        lista.add("Bogus");
+        lista.add("Arek");
+
+        for (int i = 0; i < lista.size(); i++){
+            System.out.println(lista.get(i));
+        }
+
 
     }
 }
